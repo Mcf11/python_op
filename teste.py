9b9c841a7b7c8d143f64abc19732c6fcac66880b
Mcf11 <michel.felipe0@hotmail.com>
2024-10-18 16:32:09 -0300

    Just for search out of my house

---
+++ teste.py
@@ -0,0 +1,15 @@
+meu_salario = 100
+imposto = 0.1
+meus_impostos = meu_salario * imposto
+print(meus_impostos)
+
+word = "ABCDEFGHIJK"
+print(word[0])    #pega uma letra só
+print(word[2:])   #do index 2 pra frente
+print(word[:6])   #até o index6 (não incluindo ele)
+print(word[2:6])  #do index 2 ao 6
+print(word[1:3])  #inclui o index 1 e vai até o index 2, parando um index antes do informado
+print(word[::2])  #all the way from beginning to the end jumping from 2 to 2
+
+print("Hello world"[8])
+
